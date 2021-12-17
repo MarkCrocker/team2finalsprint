569db074435c589af71007bcc7f01330d07bd70f
Daniel Harris <daniellucasharris@hotmail.com>
2021-12-17 02:20:23 -0330

    Add Thymeleaf dependency, create WebController to handle templates

--- src/main/java/com/team2/FinalSprint/Web/WebController.java
+++ src/main/java/com/team2/FinalSprint/Web/WebController.java
@@ -1,4 +1,20 @@
 package com.team2.FinalSprint.Web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
 public class WebController {
+
+    @RequestMapping(value = "/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping(value = "/login")
+    public String login(){
+        return "login";
+    }
+
 }
