47c78306847634883359befc06016bd1b675f6c8
Mark Crocker <mcrocker@Marks-MacBook-Pro.local>
2021-12-19 15:42:34 -0330

    added postgresObject

--- src/main/java/com/team2/FinalSprint/Data/PostgreSQL/PostgresObject.java
+++ src/main/java/com/team2/FinalSprint/Data/PostgreSQL/PostgresObject.java
@@ -1,4 +1,60 @@
 package com.team2.FinalSprint.Data.PostgreSQL;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class PostgresObject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column
+    private String company;
+    @Column
+    private String city;
+    @Column
+    private String jobOpening;
+    @Column
+    private String contact;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getJobOpening() {
+        return jobOpening;
+    }
+
+    public void setJobOpening(String jobOpening) {
+        this.jobOpening = jobOpening;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
 }
